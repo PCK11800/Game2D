9be4d0a245a5bc01805dead9770556bba2b57d68
HarrisDWE <47992021+HarrisDWE@users.noreply.github.com>
2020-03-03 15:26:49 +0000

    Another Attempted Shop Screen Fix for Lower Res Screens

--- TanksV1/src/Tanks/UIScreens/ShopScreen.java
+++ TanksV1/src/Tanks/UIScreens/ShopScreen.java
@@ -35,16 +35,16 @@ public class ShopScreen extends UIScreen
     {
 
         //Upgrade buttons - Top Row
-        addUpgradeButton(getXPos(0), upgradeButtonHeight, upgradeButtonWidth, upgradeButtonHeight, Textures.MAXHEALTH, Textures.MAXHEALTH_HOVER, Textures.MAXHEALTH_CLICKED, player, "increase_maxhealth", 75);
-        addUpgradeButton(getXPos(1), upgradeButtonHeight, upgradeButtonWidth, upgradeButtonHeight, Textures.HALFHEALTH, Textures.HALFHEALTH_HOVER, Textures.HALFHEALTH_CLICKED, player, "halfheal", 50);
-        addUpgradeButton(getXPos(2), upgradeButtonHeight, upgradeButtonWidth, upgradeButtonHeight, Textures.HEALTH, Textures.HEALTH_HOVER, Textures.HEALTH_CLICKED, player, "fullheal", 90);
-        addUpgradeButton(getXPos(3), upgradeButtonHeight, upgradeButtonWidth, upgradeButtonHeight, Textures.SPIKES, Textures.SPIKES_HOVER, Textures.SPIKES_CLICKED, player, "spikes_upgrade", 250);
+        addUpgradeButton(getXPos(0), upgradeButtonHeight / 2, upgradeButtonWidth, upgradeButtonHeight, Textures.MAXHEALTH, Textures.MAXHEALTH_HOVER, Textures.MAXHEALTH_CLICKED, player, "increase_maxhealth", 75);
+        addUpgradeButton(getXPos(1), upgradeButtonHeight / 2, upgradeButtonWidth, upgradeButtonHeight, Textures.HALFHEALTH, Textures.HALFHEALTH_HOVER, Textures.HALFHEALTH_CLICKED, player, "halfheal", 50);
+        addUpgradeButton(getXPos(2), upgradeButtonHeight / 2, upgradeButtonWidth, upgradeButtonHeight, Textures.HEALTH, Textures.HEALTH_HOVER, Textures.HEALTH_CLICKED, player, "fullheal", 90);
+        addUpgradeButton(getXPos(3), upgradeButtonHeight / 2, upgradeButtonWidth, upgradeButtonHeight, Textures.SPIKES, Textures.SPIKES_HOVER, Textures.SPIKES_CLICKED, player, "spikes_upgrade", 250);
 
         //Upgrade buttons - Second
-        addUpgradeButton(getXPos(0), (upgradeButtonHeight * 2) + 100, upgradeButtonWidth, upgradeButtonHeight, Textures.MINIGUN, Textures.MINIGUN_HOVER, Textures.MINIGUN_CLICKED, player, "minigun_upgrade", 200);
-        addUpgradeButton(getXPos(1), (upgradeButtonHeight * 2) + 100, upgradeButtonWidth, upgradeButtonHeight, Textures.RAILGUN, Textures.RAILGUN_HOVER, Textures.RAILGUN_CLICKED, player, "railgun_upgrade", 300);
-        addUpgradeButton(getXPos(2), (upgradeButtonHeight * 2) + 100, upgradeButtonWidth, upgradeButtonHeight, Textures.SONIC, Textures.SONIC_HOVER, Textures.SONIC_CLICKED, player, "sonicmode_upgrade", 150);
-        addUpgradeButton(getXPos(3), (upgradeButtonHeight * 2) + 100, upgradeButtonWidth, upgradeButtonHeight, Textures.ARMOUR, Textures.ARMOUR_HOVER, Textures.ARMOUR_CLICKED, player, "armour_upgrade", 200);
+        addUpgradeButton(getXPos(0), (upgradeButtonHeight * 2), upgradeButtonWidth, upgradeButtonHeight, Textures.MINIGUN, Textures.MINIGUN_HOVER, Textures.MINIGUN_CLICKED, player, "minigun_upgrade", 200);
+        addUpgradeButton(getXPos(1), (upgradeButtonHeight * 2), upgradeButtonWidth, upgradeButtonHeight, Textures.RAILGUN, Textures.RAILGUN_HOVER, Textures.RAILGUN_CLICKED, player, "railgun_upgrade", 300);
+        addUpgradeButton(getXPos(2), (upgradeButtonHeight * 2), upgradeButtonWidth, upgradeButtonHeight, Textures.SONIC, Textures.SONIC_HOVER, Textures.SONIC_CLICKED, player, "sonicmode_upgrade", 150);
+        addUpgradeButton(getXPos(3), (upgradeButtonHeight * 2), upgradeButtonWidth, upgradeButtonHeight, Textures.ARMOUR, Textures.ARMOUR_HOVER, Textures.ARMOUR_CLICKED, player, "armour_upgrade", 200);
 
         //Continue Button
         addLoadLevelButton(screenWidth - 250, screenHeight - 100, continueButtonWidth, continueButtonHeight, Textures.CONTINUE, Textures.CONTINUE_HOVER, Textures.CONTINUE_CLICKED);
@@ -57,22 +57,23 @@ public class ShopScreen extends UIScreen
 
     private float getXPos(int i)
     {
-        int xOffset = 50;
-        int buttonPadding = 100;
+        float xScale = getWindow().getSize().x / getWindow().getWidth();
+        float xOffset = 50;
+        float buttonPadding = 100;
 
         switch (i)
         {
             case 0:
-                return  (xOffset + (upgradeButtonWidth * i) + (upgradeButtonWidth / 2));
+                return xScale * (xOffset + (upgradeButtonWidth * i) + (upgradeButtonWidth / 2));
 
             case 1:
-                return (xOffset + buttonPadding + (upgradeButtonWidth * i) + (upgradeButtonWidth / 2));
+                return xScale * (xOffset + buttonPadding + (upgradeButtonWidth * i) + (upgradeButtonWidth / 2));
 
             case 2:
-                return (this.getWindow().getSize().x - (xOffset + buttonPadding + upgradeButtonWidth + (upgradeButtonWidth / 2)));
+                return  xScale * (this.getWindow().getSize().x - (xOffset + buttonPadding + upgradeButtonWidth + (upgradeButtonWidth / 2)));
 
             case 3:
-                return (this.getWindow().getSize().x - (xOffset + (upgradeButtonWidth / 2)));
+                return xScale * (this.getWindow().getSize().x - (xOffset + (upgradeButtonWidth / 2)));
 
             default:
                 return xOffset + upgradeButtonWidth;
