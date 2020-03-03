8098978cf83fa3d85066b3e52991920620161bb2
HarrisDWE <47992021+HarrisDWE@users.noreply.github.com>
2020-03-03 13:21:57 +0000

    Implemented Fix for Relative Tank Turret Aiming
    Implemented the fix to make the tank turret relative to the window

--- TanksV1/src/Tanks/ObjectComponents/TankTurret.java
+++ TanksV1/src/Tanks/ObjectComponents/TankTurret.java
@@ -2,53 +2,56 @@ package Tanks.ObjectComponents;
 
 import java.awt.MouseInfo;
 import Tanks.Window.Window;
+import org.jsfml.system.Vector2i;
+import org.jsfml.window.Mouse;
 
 public class TankTurret extends RotatingObject {
 
 	private TankHull connectedTankHull;
 	protected float turningDistance;
 	protected Window window;
-	
-	public void setTurretLocation() 
+
+	public void setTurretLocation()
 	{
 		float xPos = connectedTankHull.getxPos();
 		float yPos = connectedTankHull.getyPos();
 		setCenterLocation(xPos, yPos);
 	}
-	
-	public void rotateRight() 
+
+	public void rotateRight()
 	{
 		rotateObject(objectDirection + turningDistance);
 		if(objectDirection >= 360) {
 			objectDirection = objectDirection - 360;
 		}
 	}
-	
-	public void rotateLeft() 
+
+	public void rotateLeft()
 	{
 		rotateObject(objectDirection - turningDistance);
 		if(objectDirection <= 0) {
 			objectDirection = objectDirection + 360;
 		}
 	}
-	
-	public void setPlayerTurretDirection() 
+
+	public void setPlayerTurretDirection()
 	{
-		 float mouseXPos = MouseInfo.getPointerInfo().getLocation().x;
-	     float mouseYPos = MouseInfo.getPointerInfo().getLocation().y;
+		// This sets the mouse position to be relative to the window
+		float mouseXPos = Mouse.getPosition(this.window).x; //MouseInfo.getPointerInfo().getLocation().x;
+		float mouseYPos = Mouse.getPosition(this.window).y; //MouseInfo.getPointerInfo().getLocation().y;
 
-	     double dx = connectedTankHull.getxPos() - mouseXPos;
-	     double dy = connectedTankHull.getyPos() - mouseYPos;
+		double dx = connectedTankHull.getxPos() - mouseXPos;
+		double dy = connectedTankHull.getyPos() - mouseYPos;
 
-	     double rotationAngle = Math.toDegrees(Math.atan2(dy, dx));
-	     if(rotationAngle >= 360) {
-	    	 rotationAngle = rotationAngle - 360;
-	     }
-	     else if(rotationAngle <= 0) {
-	    	 rotationAngle = rotationAngle + 360;
-	     }
+		double rotationAngle = Math.toDegrees(Math.atan2(dy, dx));
+		if(rotationAngle >= 360) {
+			rotationAngle = rotationAngle - 360;
+		}
+		else if(rotationAngle <= 0) {
+			rotationAngle = rotationAngle + 360;
+		}
 
-	     rotateObject((float) rotationAngle - 90);
+		rotateObject((float) rotationAngle - 90);
 	}
 
 
@@ -71,28 +74,28 @@ public class TankTurret extends RotatingObject {
 	}
 
 
-	public void setConnectedTankHull(TankHull connectedTankHull) 
+	public void setConnectedTankHull(TankHull connectedTankHull)
 	{
 		this.connectedTankHull = connectedTankHull;
 	}
-	
+
 	public float getTurningDistance()
 	{
 		return turningDistance;
 	}
-	
-	public void setTurningDistance(float turningDistance) 
+
+	public void setTurningDistance(float turningDistance)
 	{
 		this.turningDistance = turningDistance;
 	}
-	
-	public void setWindow(Window window) 
+
+	public void setWindow(Window window)
 	{
 		this.window = window;
 	}
 
 	public float getDirection() { return this.objectDirection; }
-	
+
 	public void update() { draw(window); }
 
 	public float getXPos() { return getxPos(); }
