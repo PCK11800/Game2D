5f751600d5f306c0a65a71c2894edb1df3e61cf8
Casey Pak <casemcpak@gmail.com>
2020-03-05 12:48:50 +0000

    Resize Fix

--- TanksV1/src/Tanks/Objects/ObjectSizeHandler.java
+++ TanksV1/src/Tanks/Objects/ObjectSizeHandler.java
@@ -12,8 +12,14 @@ public class ObjectSizeHandler {
         float[] scale = new float[2];
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        scale[0] = (float) (screenSize.getWidth()/defaultWidth);
-        scale[1] = (float) (screenSize.getHeight()/defaultHeight);
+
+        if(screenSize.getWidth() < defaultWidth){
+            scale[0] = (float) (screenSize.getWidth()/defaultWidth);
+        } else { scale[0] = 1; }
+
+        if(screenSize.getHeight() < defaultHeight){
+            scale[1] = (float) (screenSize.getHeight()/defaultHeight);
+        } else { scale[1] = 1; }
 
         return scale;
     }
